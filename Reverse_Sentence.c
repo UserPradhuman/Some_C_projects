fd44fa1ef33315ec04b87f27749f5f73d1ebeb03
UserPradhuman <45412369+UserPradhuman@users.noreply.github.com>
2025-12-30 20:32:48 +0530

    adding files

---
+++ Reverse_Sentence.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+
+    char A[]="Hi  my name  is Faloo.";
+    char **B;
+    for(int i=0;i<strlen(A);++i){
+
+        if((122>A[i] && A[i]>97) || (90>A[i] && A[i]>65)){
+
+            
+
+        }
+    }
+
+    return 0;
+}
